43c2ed952d0edffad34b9d82151d019870a5ab8d
Nikita <nikita2003borovik@gmail.com>
2021-05-26 16:20:23 +0300

    Spaceship class comleted

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -1,16 +1,23 @@
 public class SpaceShip extends GameObject{
-    App app;
     public SpaceShip() {
 
     }
     public void Init(){
         setSprite("SpaceShacal.png");
+        setX(App.getWIDTH()/2);
+        setY(App.getHEIGHT()/2);
+        setTag(Tag.player);
 
     }
-    public void Update(){
-    }
-    public void onCollision(){
 
+    public void onCollision(GameObject other){
+        if(other.getTag()==Tag.asteroid){
+            GameEngine.stopGame();
+        }else if(other.getTag()==Tag.astronaut){
+            SceneManager.setScore(SceneManager.getScore()+5);
+        }else if(other.getTag()==Tag.pickup){
+            ((Pickup)(other)).doSmth();
+        }
     }
-    public void
+
 }
