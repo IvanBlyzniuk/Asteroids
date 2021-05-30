9f0cf305856f2deced8e29f400cce86d4e740abb
Ivan Blyzniuk <ivan.blyzniuk@ukma.edu.ua>
2021-05-30 14:22:09 +0300

    Added input for shooting

--- src/App.java
+++ src/App.java
@@ -70,6 +70,7 @@ public class App extends Application {
                 if(inputs.contains("A")) LevelManager.getManager().onLeftPressed();
                 if(inputs.contains("D")) LevelManager.getManager().onRightPressed();
                 if(inputs.contains("S")) LevelManager.getManager().onDownPressed();
+                if(inputs.contains("SPACE")) LevelManager.getManager().onShootPressed();
                 GameEngine.update();
             }
         };
