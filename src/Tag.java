92015500a11800d231a528439e72b7a83d178f0e
Ivan Blyzniuk <ivan.blyzniuk@ukma.edu.ua>
2021-06-06 19:38:43 +0300

    Fixed window size issue

--- src/Tag.java
+++ src/Tag.java
@@ -1,5 +1,3 @@
 public enum Tag {
     bullet,player,asteroid,astronaut,pickup,big,small,teleFraggable,rocketPickup,protectionPickup,tripleShotPickup,rocket, countsTowardsCap,addOneRocketPickup,explosion
-
-
 }
